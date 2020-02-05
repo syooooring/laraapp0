57295653bd9f2775d9bdfc09c3a781e48b234322
syooooring <syooooring@gmail.com>
2020-02-05 17:46:39 +0900

    validate

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -13,6 +13,13 @@ class BbsController extends Controller
 
   public function create(Request $request)
   {
+
+    //バリデーションチェック
+    $request->validate([
+      'name' => 'required|max:10',
+      'comment' => 'required|min:5|max:150',
+    ]);
+
     // 投稿内容を受け取って変数に入れる
     $name = $request->input('name');
     $comment = $request->input('comment');
